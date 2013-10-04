59cefc2185a9bfb4a8d0508458e9a561e8cc3262
Daniel Stockman <daniels@zillow.com>
2013-10-04 16:54:45 -0700

    Change dynamic index imports to CapCase, they're constructors.

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ Avoid invoking cluster stuff when using programmatically.
 exports = module.exports = require("./lib/less-cluster");
 
 // make it available dynamically if they really, really want it.
-["master", "worker"].forEach(function (name) {
+["Master", "Worker"].forEach(function (name) {
     Object.defineProperty(exports, name, {
         get: function () {
-            return require("./lib/less-" + name);
+            return require("./lib/" + name.toLowerCase());
         }
     });
 });
