b5fe8d847e0a5e381cc05ca488698c48369c88bd
Daniel Stockman <daniels@zillow.com>
2013-10-04 13:37:29 -0700

    Switch Logger to parasitic inheritance, making classes a bit more idiomatic in construction.

--- test/test-logger.js
+++ test/test-logger.js
@@ -1,11 +1,46 @@
 /**
 Tests for the logger mixin
 **/
+var assert = require('assert');
+var inherits = require('util').inherits;
 var vows = require('vows');
 
 var Logger = require('../lib/logger');
 
 vows.describe('Logger').addBatch({
+    "##mixin()": {
+        topic: function () {
+            function Super() {}
+            Super.prototype.log = function () {
+                this.logged = "super";
+            };
+
+            function Klass(options) {
+                Super.call(this);
+                Logger.call(this, options);
+            }
+
+            inherits(Klass, Super);
+            Logger.mixin(Klass);
+
+            return {
+                Super: Super,
+                Klass: Klass
+            };
+        },
+        "should provide all methods": function (topic) {
+            var instance = new topic.Klass();
+            assert.ok(instance.debug);
+            assert.ok(instance.log);
+            assert.ok(instance.warn);
+            assert.ok(instance.error);
+        },
+        "should shadow superclass methods": function (topic) {
+            var instance = new topic.Klass();
+            instance.log();
+            assert.strictEqual(instance.logged, undefined);
+        }
+    },
     "debug()": {
         topic: new Logger(),
         "should NOT emit without --verbose": function (topic) {
